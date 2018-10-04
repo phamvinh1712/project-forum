a8bc74e475aeb400be7717b7406d1bf189a6d38f
phamvinh1712 <phamvinh1712@gmail.com>
2018-10-04 13:57:11 +0700

    add routing

---
+++ src/components/Test.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+//This is example component for routing
+class Test extends Component {
+    render() {
+        return (
+            <div>
+                Test
+            </div>
+        );
+    }
+}
+
+export default Test;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {
+    BrowserRouter,
+    Route,
+    Link
+} from 'react-router-dom';
 import App from './App';
+import Test from './components/Test';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <div>
+            <Route exact path="/" component={App}/>
+            <Route path="/test" component={Test}/>
+            //Add route here
+        </div>
+    </BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
