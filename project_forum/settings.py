b01a71d69fa281b0c45a1d8fc696ea4de931642d
phamvinh1712 <phamvinh1712@gmail.com>
2018-10-10 13:36:07 +0700

    edit typo

--- project_forum/settings.py
+++ project_forum/settings.py
@@ -39,11 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'api',
     'corsheaders'
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
