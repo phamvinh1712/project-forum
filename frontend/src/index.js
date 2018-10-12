c42c98a255219b48de93a9b7b161ee96cb2accdf
haphananhtuan <haphan613@gmail.com>
2018-10-12 20:53:25 +0700

    Add login and registration
    Signed-off-by: haphananhtuan <haphan613@gmail.com>

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {
+    BrowserRouter,
+    Route,
+    Link
+} from 'react-router-dom';
+import Login from "./components/Login";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Route exact path="/" component={App}/>
+      <Route path="/login" component={Login}/>
+
+    </div>
+  </BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
