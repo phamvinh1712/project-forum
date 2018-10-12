d17a230f52ca7547e5cccec43c627ca4bbd3b36f
haphananhtuan <haphan613@gmail.com>
2018-10-12 13:04:43 +0700

    Add login and registration
    Signed-off-by: haphananhtuan <haphan613@gmail.com>

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,8 @@
-import React, { Component } from "react";
-import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import React, {Component} from "react";
+import {Button, FormGroup, FormControl, ControlLabel} from "react-bootstrap";
 import "./Login.css";
+import {createStore} from 'redux'
+import {connect} from "react-redux";
 // login page
 export default class Login extends Component {
   constructor(props) {
@@ -11,10 +13,12 @@ export default class Login extends Component {
       password: ""
     };
   }
+
 // check if the user input correct form
   validateForm() {
     return this.state.email.length > 0 && this.state.password.length > 0;
   }
+
 // function to handle value change in field
   handleChange = event => {
     this.setState({
@@ -24,7 +28,37 @@ export default class Login extends Component {
 // function handle submit onclick event
   handleSubmit = event => {
     event.preventDefault();
+    let us = this.state.email;
+    let ps = this.state.password;
+    let headers = {"Content-Type": "application/json"};
+    let body = JSON.stringify({us, ps,});
+
+    return fetch("/api/rest-auth/login/", {headers, body, method: "POST"})
+      .then(res => {
+        if (res.status < 500) {
+          return res.json().then(data => {
+            return {status: res.status, data};
+          })
+        } else {
+          console.log("Server Error!");
+          throw res;
+        }
+      })
+      .then(res => {
+        if (res.status === 200) {
+          dispatch({type: 'LOGIN_SUCCESSFUL', data: res.data});
+          return res.data;
+        } else if (res.status === 403 || res.status === 401) {
+          dispatch({type: "AUTHENTICATION_ERROR", data: res.data});
+          throw res.data;
+        } else {
+          dispatch({type: "LOGIN_FAILED", data: res.data});
+          throw res.data;
+        }
+      })
+
   }
+
 // main screen
   render() {
     return (
