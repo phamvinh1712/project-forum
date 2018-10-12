c1910b5d3c510a0b43a7dfd9ce0456627eda13bd
haphananhtuan <haphan613@gmail.com>
2018-10-12 22:25:54 +0700

    Add login and registration
    Signed-off-by: haphananhtuan <haphan613@gmail.com>

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,20 +1,23 @@
-import React, { Component } from "react";
-import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import React, {Component} from "react";
+import {Button, FormGroup, FormControl, ControlLabel} from "react-bootstrap";
 import "./Login.css";
-// login page
+import auth from "./auth"//
+// gin page
 export default class Login extends Component {
   constructor(props) {
     super(props);
-// value of data
+// valu loe of data
     this.state = {
       email: "",
       password: ""
     };
   }
+
 // check if the user input correct form
   validateForm() {
     return this.state.email.length > 0 && this.state.password.length > 0;
   }
+
 // function to handle value change in field
   handleChange = event => {
     this.setState({
@@ -24,7 +27,17 @@ export default class Login extends Component {
 // function handle submit onclick event
   handleSubmit = event => {
     event.preventDefault();
+    const em = this.state.email;
+    const ps = this.state.password;
+    auth.login(em, ps, (loggedIn) => {
+      if (loggedIn) {
+        console.log("success")
+      } else {
+        console.log("Failed")
+      }
+    })
   }
+
 // main screen
   render() {
     return (
