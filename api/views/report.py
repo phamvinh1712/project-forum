d820ba1acdc57f132f7572fed96dc051f4305ea7
unknown <hoang19101997@gmail.com>
2018-10-17 21:04:54 +0700

    post request for report

--- api/views/report.py
+++ api/views/report.py
@@ -6,9 +6,19 @@ from ..models import Report
 class CreateReport():
     def createpost(req):
      if req.method == 'POST':
-        Post = Report()
-        Post.reason = req.POST.get('reason')
-        Post.save()   
+         Post = Report()       
+         if req.POST.get('comment'):
+            Post.comment = req.POST.get('comment')
+         if req.POST.get('hashtag'):
+            Post.hashtag = req.POST.get('hashtag')
+         if req.POST.get('post'):
+            Post.post = req.POST.get('post')
+         if req.POST.get('reply'):
+            Post.reply = req.POST.get('reply')
+         Post.user = req.POST.get('user')
+         Post.create_time = req.POST.get('create_time')   
+         Post.reason = req.POST.get('reason')
+         Post.save()   
 
 class RepostListView(generics.ListAPIView):
     queryset = Report.objects.all()
