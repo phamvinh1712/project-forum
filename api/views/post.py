074684dec1b5ffdbbe5dd0d98b20b6db1a98c5d9
haphananhtuan <haphan613@gmail.com>
2018-10-17 13:14:45 +0700

    Merge branch 'master' of https://github.com/phamvinh1712/project-forum
    Signed-off-by: haphananhtuan <haphan613@gmail.com>
    
    # Conflicts:
    #	api/serializers/__init__.py
    #	api/serializers/post.py
    #	api/serializers/sub_thread.py
    #	api/urls.py
    #	api/views/__init__.py
    #	api/views/post.py
    #	frontend/package-lock.json
    #	frontend/src/components/Login.js
    #	frontend/src/components/NavBar.js

---
+++ api/views/post.py
@@ -0,0 +1,18 @@
+from rest_framework import generics
+from ..models import Post
+from ..serializers import PostSerializer, PostDetailSerializer
+from rest_framework.permissions import IsAuthenticated
+
+
+class CreatePostView(generics.CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = IsAuthenticated,
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class PostView(generics.RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostDetailSerializer
