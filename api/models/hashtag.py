b292c3679579bbe285cec45d4b52f48718428c74
khoaphung <khoa.na.phung@gmail.com>
2018-10-17 16:02:36 +0700

    delete

--- api/models/hashtag.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-from datetime import datetime
-
-
-class Hashtag(models.Model):
-    name = models.CharField(max_length=20, unique=True, )
-    create_time = models.DateTimeField(default=datetime.now)
-    create_user = models.ForeignKey(User, on_delete=models.CASCADE)
