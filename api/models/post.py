a50109acebbaa82007fe4b4f4b20186750141f02
unknown <hoang19101997@gmail.com>
2018-10-19 14:07:26 +0700

    a

--- api/models/post.py
+++ api/models/post.py
@@ -6,12 +6,12 @@ from datetime import datetime
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=50, null = True)
-    content = models.TextField( null = True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null = True)
-    sub_thread = models.ForeignKey(SubThread, on_delete=models.CASCADE,related_name="post", null = True)
-    view_count = models.IntegerField(default=0, null = True)
-    create_time = models.DateTimeField(default=datetime.now, null = True)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    sub_thread = models.ForeignKey(SubThread, on_delete=models.CASCADE,related_name="post")
+    view_count = models.IntegerField(default=0)
+    create_time = models.DateTimeField(default=datetime.now)
     hashtags = models.ManyToManyField(Hashtag, blank=True) 
 
     class Meta:
