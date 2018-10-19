efe37252dd3d366a397ceb87e0af484b9274c8c8
unknown <hoang19101997@gmail.com>
2018-10-19 14:51:41 +0700

    change

--- api/serializers/comment.py
+++ api/serializers/comment.py
@@ -4,24 +4,15 @@ from ..serializers import UserDetailSerializer
 
 
 class CommentSerializer(serializers.ModelSerializer):
-<<<<<<< HEAD
-    user = UserDetailSerializer(read_only = True)
-=======
     user = UserDetailSerializer(read_only=True)
 
->>>>>>> a681c4027953fcfc298be3b784e6fce23f91d36e
     class Meta:
         model = Comment
         fields = '__all__'
 
-<<<<<<< HEAD
-class CreateCommentSerializer(serializers.ModelSerializer):
-    
-=======
 
 class CreateCommentSerializer(serializers.ModelSerializer):
 
->>>>>>> a681c4027953fcfc298be3b784e6fce23f91d36e
     class Meta:
         model = Comment
         exclude = ('user',)
