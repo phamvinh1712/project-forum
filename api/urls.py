4809e8cd6de96ad42e2424936b88f777e5ce6f4e
phungkhoa <43835508+phungkhoa@users.noreply.github.com>
2018-10-20 11:10:28 +0700

    Add files via upload
    20Oct editUser

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,28 @@
 from django.urls import include, path, re_path
 from .views import *
+from rest_auth.registration.views import VerifyEmailView
+from django.urls import include, path
 
 urlpatterns = [
-    #path('user/', ListPostDetailView.as_view()),
-    path('post/', PostView.as_view()),
+    re_path(r'^rest-auth/registration/account-confirm-email/(?P<key>[-:\w]+)/$', EmailConfirmView.as_view(),
+            name='account_confirm_email'),
+    re_path(r'^rest-auth/registration/account-confirm-email/', VerifyEmailView.as_view(),
+            name='account_email_verification_sent'),
+    path('rest-auth/', include('rest_auth.urls')),
+    re_path(r'^rest-auth/registration/$', NameRegistrationView.as_view(), name="rest_name_register"),
+    path('hashtags/', HashtagListView.as_view()),
+    path('user-detail/', UserDetailView.as_view()),
+    path('notifications/', NotificationListView.as_view()),
+    path('create-post/', CreatePostView.as_view()),
+    re_path(r'^posts/(?P<pk>[0-9]+)/$', PostView.as_view()),
+    path('thread/', ThreadListView.as_view()),
+    path('report/', RepostListView.as_view()),
+    re_path(r'^subthread/(?P<pk>[0-9]+)/$', SubThreadDetailView.as_view(), name="subthread-detail"),
+    path('notifications/', NotificationListView.as_view()),
+    re_path(r'^subthread/(?P<pk>[0-9]+)/posts/$', ListPostDetailView.as_view(), name="postlist-detail"),
+    re_path(r'^comments/(?P<pk>[0-9]+)/replies/$', ReplyView.as_view()),
+    path('create-comment/', CreateCommentView.as_view()),
+    path('create-reply/', CreateReplyView.as_view()),
     path('post/<int:id>/delete', delete_post),
-    #re_path(r'^post/(?P<pk>+)/delete/$', delete_post, name='delete_post'),
+    re_path(r'^users/(?P<pk>\d+)/$', EditUser.as_view()),
 ]
