f5cadac4d88116f3fd11d24f4c0c0e17e8d71a43
phungkhoa <43835508+phungkhoa@users.noreply.github.com>
2018-10-22 13:36:52 +0700

    Update sub_thread.py

--- api/serializers/sub_thread.py
+++ api/serializers/sub_thread.py
@@ -1,38 +1,18 @@
 from rest_framework import serializers
 from ..models import SubThread
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-
-class SubThreadSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = SubThread
-		fields = "__all__"
-=======
->>>>>>> Khoa
 from ..models import Post
 from ..serializers import UserDetailSerializer
 
-
-<<<<<<< HEAD
 class SubThreadSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SubThread
-        fields = "__all__"
-=======
+	post = PostSerializer(many=True)
+	
+	class Meta:
+		model = SubThread
+		fields = "__all__"
+
 class PostSerializer(serializers.ModelSerializer):
     user = UserDetailSerializer()
 
     class Meta:
         model = Post
         fields = '__all__'
-
-
-class SubThreadSerializer(serializers.ModelSerializer):
-    post = PostSerializer(many=True)
-
-    class Meta:
-        model = SubThread
-        fields = "__all__"
->>>>>>> 3d9facaf0944628c49b0f95d1d8ee50816f3364b
->>>>>>> Khoa
