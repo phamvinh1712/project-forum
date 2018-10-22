4c219d310e0514ba969832957e61d2e99b66408a
phungkhoa <43835508+phungkhoa@users.noreply.github.com>
2018-10-22 13:42:18 +0700

    Update sub_thread.py

--- api/views/sub_thread.py
+++ api/views/sub_thread.py
@@ -1,5 +1,4 @@
 from rest_framework import generics
-<<<<<<< HEAD
 from ..models import SubThread
 from ..serializers import SubThreadSerializer
 from rest_framework import pagination
@@ -8,13 +7,4 @@ from rest_framework import pagination
 class SubThreadDetailView(generics.ListAPIView):
     serializer_class = SubThreadSerializer
     pagination_class = pagination.LimitOffsetPagination
-=======
-from rest_framework.response import Response
-from ..models import SubThread
-from ..serializers import SubThreadSerializer
-
-
-class SubThreadDetailView(generics.RetrieveAPIView):
-    serializer_class = SubThreadSerializer
->>>>>>> Khoa
     queryset = SubThread.objects.all()
