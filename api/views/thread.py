9cda85c7942043232fc9dee65f430706bf20fc1c
phungkhoa <43835508+phungkhoa@users.noreply.github.com>
2018-10-22 13:43:00 +0700

    Update thread.py

--- api/views/thread.py
+++ api/views/thread.py
@@ -2,14 +2,6 @@ from rest_framework import generics
 from ..models import Thread
 from ..serializers import ThreadSerializer
 
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
-=======
-
->>>>>>> 3d9facaf0944628c49b0f95d1d8ee50816f3364b
->>>>>>> Khoa
 class ThreadListView(generics.ListAPIView):
     serializer_class = ThreadSerializer
 
