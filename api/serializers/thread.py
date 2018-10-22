b082517786f35a5319267d141782a110b21d23b7
phungkhoa <43835508+phungkhoa@users.noreply.github.com>
2018-10-22 13:38:22 +0700

    Update thread.py

--- api/serializers/thread.py
+++ api/serializers/thread.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-<<<<<<< HEAD
 from ..models import Thread, SubThread
 
 
@@ -15,26 +14,4 @@ class ThreadSerializer(serializers.ModelSerializer):
     class Meta:
         model = Thread
         fields = '__all__'
-=======
-from ..models import Thread
-<<<<<<< HEAD
-from ..models import SubThread
 
-class SubThreadSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = SubThread
-		fields = '__all__'
-
-class ThreadSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Thread
-		fields = '__all__'
-=======
-
-
-class ThreadSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Thread
-        fields = '__all__'
->>>>>>> 3d9facaf0944628c49b0f95d1d8ee50816f3364b
->>>>>>> Khoa
